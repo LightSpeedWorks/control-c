4bbf7b1c4af0abf8abaa4b75848b05313479933f
Kazuaki Nishizawa <Kazuaki.Nishizawa@gmail.com>
2014-11-05 11:57:43 +0900

    modify test scripts

--- test/general-test.js
+++ test/general-test.js
@@ -11,7 +11,7 @@
 
   var c1 = new ControlC(
     function () { console.log('c1: 1 time'); },
-    function () { console.log('c1: 2 times');
+    function () { console.log('c1: 2 times -> remove');
                   this.remove(); },
     function () { console.log('c1: 3 times'); },
     function () { console.log('c1: 4 times or more -> exit');
@@ -21,14 +21,14 @@
   var c2 = ControlC(
     function () { console.log('c2: 1 time'); },
     function () { console.log('c2: 2 times'); },
-    function () { console.log('c2: 3 times');
+    function () { console.log('c2: 3 times -> remove');
                   this.remove(); },
     function () { console.log('c2: 4 times or more -> exit');
                   process.nextTick(process.exit); }
   );
 
   var c3 = ControlC.new(
-    function () { console.log('c3: 1 time');
+    function () { console.log('c3: 1 time -> add c1 and c2');
                   c1.add(); c2.add(); },
     function () { console.log('c3: 2 times'); },
     function () { console.log('c3: 3 times'); },
@@ -49,5 +49,5 @@
   catch(e) { console.log(e + ''); }
   console.log('ControlC.interval =', ControlC.interval);
 
-  console.log('press control-c in 20 seconds.');
-  setTimeout(function () {}, 20 * 1000);
+  console.log('press control-c in 30 seconds.');
+  setTimeout(function () {}, 30 * 1000);
